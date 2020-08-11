f84f1b58d94922aaf69a65e7dc9960db598cddf0
sbmvirdi <shubhamvirdic3@gmail.com>
2020-08-11 09:40:25 +0530

    Added:Gradients and animations

---
+++ app/src/main/java/com/geexec/cse227/FirebaseStorageDemo.java
@@ -0,0 +1,14 @@
+package com.geexec.cse227;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirebaseStorageDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_storage_demo);
+    }
+}
